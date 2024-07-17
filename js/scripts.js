d7a5d5836b42c64e4b227b25a600faef6b621b1b
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2024-07-17 15:08:24 +0200

    es rivisto totalmente

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,2 @@
-console.log('ciao')
+/*//1.rimuovere tutto il markup statico di HTML e inserire tutte le immagini dinamicamente servendoci di un array e un ciclo per far concatenare un template literal. 
+le immagini saranno nascoste ,tranne la prima ch avrà una classe specifica che la renderà visibile.
